33ab1b6f9d70c4c32e2f974e1007f339bd90eca3
Abstra208 <94720847+Abstra208@users.noreply.github.com>
2023-03-06 18:13:54 +0000

    Add dependencies

---
+++ script.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const puppeteer = require('puppeteer');
+
+app.get('/screenshot', async (req, res) => {
+  const url = req.query.url;
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+  const screenshot = await page.screenshot();
+  await browser.close();
+  res.set('Content-Type', 'image/png');
+  res.send(screenshot);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
