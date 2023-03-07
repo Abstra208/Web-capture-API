de73fabc9912ac9380de6365bb6b9d11c6224bf0
Abstra208 <94720847+Abstra208@users.noreply.github.com>
2023-03-07 14:53:11 +0000

    asdsad

--- index.js
+++ index.js
@@ -13,3 +13,22 @@ app.get('/screenshot', async (req, res) => {
   res.set('Content-Type', 'image/png');
   res.send(screenshot);
 });
+
+app.get('/', async (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <section>
+            <h1>bonjours</h1>
+        </section>
+    </body>
+    </html>
+  `)
+});
