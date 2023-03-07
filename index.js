85324fcb0d5a111cf0882d0d99c3a0f536b91dd6
Abstra208 <94720847+Abstra208@users.noreply.github.com>
2023-03-07 20:27:43 +0000

    asd

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 5000;
 const puppeteer = require('puppeteer');
 
+app.listen(port, function(){
+    console.log("Serveur")
+});
+
 app.get('/', (req, res) => {
-  res.send(`
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        <section>
-            <h1>bonjours</h1>
-        </section>
-    </body>
-    </html>
-  `);
+  res.send("allo");
 });
 
 app.get('/screenshot', async (req, res) => {
-  const url = req.query.url;
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(url);
-  const screenshot = await page.screenshot();
-  await browser.close();
-  res.set('Content-Type', 'image/png');
-  res.send(screenshot);
+    const url = req.query.url;
+    const browser = await puppeteer.launch({
+        ignoreHTTPSErrors: true
+    });
+    const page = await browser.newPage();
+    await page.goto(url);
+  
+    // Ajouter un délai de 5 secondes (5000 millisecondes)
+    await new Promise(resolve => setTimeout(resolve, 5000));
+  
+    const screenshot = await page.screenshot();
+    await browser.close();
+    res.set('Content-Type', 'image/png');
+    res.send(screenshot);
 });
