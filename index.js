fda12113fdb9b2941981f27c632584e4cdfcd6e3
Abstra208 <94720847+Abstra208@users.noreply.github.com>
2023-03-07 19:50:13 +0000

    zsc

--- index.js
+++ index.js
@@ -3,17 +3,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 const puppeteer = require('puppeteer');
 
-app.get('/screenshot', async (req, res) => {
-  const url = req.query.url;
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(url);
-  const screenshot = await page.screenshot();
-  await browser.close();
-  res.set('Content-Type', 'image/png');
-  res.send(screenshot);
-});
-
 app.get('/', (req, res) => {
   res.send(`
     <!DOCTYPE html>
@@ -31,4 +20,15 @@ app.get('/', (req, res) => {
     </body>
     </html>
   `)
+});
+
+app.get('/screenshot', async (req, res) => {
+  const url = req.query.url;
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+  const screenshot = await page.screenshot();
+  await browser.close();
+  res.set('Content-Type', 'image/png');
+  res.send(screenshot);
 });
